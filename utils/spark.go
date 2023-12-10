5a2715ad2cd9e1567e1f75157683f48812f1a2f9
lei <3096407768@qq.com>
2023-12-10 21:03:50 +0800

    日志只保留一个月，超过一个月删除

---
+++ utils/spark.go
@@ -0,0 +1,128 @@
+package utils
+
+import (
+	"Tally/config"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+/**
+ *  WebAPI 接口调用示例 接口文档（必看）：https://www.xfyun.cn/doc/spark/Web.html
+ * 错误码链接：https://www.xfyun.cn/doc/spark/%E6%8E%A5%E5%8F%A3%E8%AF%B4%E6%98%8E.html（code返回错误码时必看）
+ * @author iflytek
+ */
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+func GetConnect(b []byte) *websocket.Conn {
+	d := websocket.Dialer{
+		HandshakeTimeout: 5 * time.Second,
+	}
+	//握手并建立websocket 连接
+	conn, resp, err := d.Dial(assembleAuthUrl1(config.Config.SparkDesk.HostUrl, config.Config.SparkDesk.ApiKey, config.Config.SparkDesk.ApiSecret), nil)
+	if err != nil {
+		panic(readResp(resp) + err.Error())
+		return nil
+	} else if resp.StatusCode != 101 {
+		panic(readResp(resp) + err.Error())
+	}
+
+	go func() {
+		data := genParams1(config.Config.SparkDesk.Appid, "给你以下序列化后的json数据,帮我分析一下我的消费具体情况和你的建议，现在你是一名分析师，帮我从至少4个不同方面分析，我只要最终结论，越详细越好,这是数据："+string(b))
+		err := conn.WriteJSON(data)
+		if err != nil {
+			return
+		}
+	}()
+	return conn
+}
+
+// 生成参数
+func genParams1(appid, question string) map[string]interface{} { // 根据实际情况修改返回的数据结构和字段名
+
+	messages := []Message{
+		{Role: "user", Content: question},
+	}
+	data := map[string]interface{}{ // 根据实际情况修改返回的数据结构和字段名
+		"header": map[string]interface{}{ // 根据实际情况修改返回的数据结构和字段名
+			"app_id": appid, // 根据实际情况修改返回的数据结构和字段名
+		},
+		"parameter": map[string]interface{}{ // 根据实际情况修改返回的数据结构和字段名
+			"chat": map[string]interface{}{ // 根据实际情况修改返回的数据结构和字段名
+				"domain":      "generalv3",  // 根据实际情况修改返回的数据结构和字段名
+				"temperature": float64(0.8), // 根据实际情况修改返回的数据结构和字段名
+				"top_k":       int64(6),     // 根据实际情况修改返回的数据结构和字段名
+				"max_tokens":  int64(2048),  // 根据实际情况修改返回的数据结构和字段名
+				"auditing":    "default",    // 根据实际情况修改返回的数据结构和字段名
+			},
+		},
+		"payload": map[string]interface{}{ // 根据实际情况修改返回的数据结构和字段名
+			"message": map[string]interface{}{ // 根据实际情况修改返回的数据结构和字段名
+				"text": messages, // 根据实际情况修改返回的数据结构和字段名
+			},
+		},
+	}
+	return data // 根据实际情况修改返回的数据结构和字段名
+}
+
+// 创建鉴权url  apikey 即 hmac username
+func assembleAuthUrl1(hosturl string, apiKey, apiSecret string) string {
+	ul, err := url.Parse(hosturl)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//签名时间
+	date := time.Now().UTC().Format(time.RFC1123)
+	//date = "Tue, 28 May 2019 09:10:42 MST"
+	//参与签名的字段 host ,date, request-line
+	signString := []string{"host: " + ul.Host, "date: " + date, "GET " + ul.Path + " HTTP/1.1"}
+	//拼接签名字符串
+	sgin := strings.Join(signString, "\n")
+	// fmt.Println(sgin)
+	//签名结果
+	sha := HmacWithShaTobase64("hmac-sha256", sgin, apiSecret)
+	// fmt.Println(sha)
+	//构建请求参数 此时不需要urlencoding
+	authUrl := fmt.Sprintf("hmac username=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"", apiKey,
+		"hmac-sha256", "host date request-line", sha)
+	//将请求参数使用base64编码
+	authorization := base64.StdEncoding.EncodeToString([]byte(authUrl))
+
+	v := url.Values{}
+	v.Add("host", ul.Host)
+	v.Add("date", date)
+	v.Add("authorization", authorization)
+	//将编码后的字符串url encode后添加到url后面
+	callurl := hosturl + "?" + v.Encode()
+	return callurl
+}
+
+func HmacWithShaTobase64(algorithm, data, key string) string {
+	mac := hmac.New(sha256.New, []byte(key))
+	mac.Write([]byte(data))
+	encodeData := mac.Sum(nil)
+	return base64.StdEncoding.EncodeToString(encodeData)
+}
+
+func readResp(resp *http.Response) string {
+	if resp == nil {
+		return ""
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("code=%d,body=%s", resp.StatusCode, string(b))
+}
